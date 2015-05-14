b0378c09fc614b9c897c81d5ec4ab42ae87386b6
zhouyichen <zhouyichen@users.noreply.github.com>
2015-05-14 17:07:17 +0800

    read xlsx and output array
    Read a xlsx file and convert it to a associative array, with the sheet
    name being the key, and a 2d array representing the cell values

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function fixdata(data) {
+    var o = "", l = 0, w = 10240;
+    for(; l<data.byteLength/w; ++l) o+=String.fromCharCode.apply(null,new Uint8Array(data.slice(l*w,l*w+w)));
+    o+=String.fromCharCode.apply(null, new Uint8Array(data.slice(l*w)));
+    return o;
+}
+
+function to_array(workbook) {
+    var result = [];
+    workbook.SheetNames.forEach(function(sheetName) {
+        var csv = XLSX.utils.sheet_to_csv(workbook.Sheets[sheetName]);
+        array = csv.csvToArray({rSep:'\n'});
+        if(csv.length > 0){
+            result[sheetName] = array;
+        }
+    });
+    return result;
+}
+
+var xlf = document.getElementById('xlf');
+
+function handleFile(e) {
+    var files = e.target.files;
+    var f = files[0];
+    {
+        var reader = new FileReader();
+        var name = f.name;
+        reader.onload = function(e) {
+            var data = e.target.result;
+            var wb;
+            var arr = fixdata(data);
+            wb = XLSX.read(btoa(arr), {type: 'base64'});
+            array = to_array(wb);
+            // console.log(array);
+        };
+        reader.readAsArrayBuffer(f);
+    }
+}
+
+xlf.addEventListener('change', handleFile, false);
