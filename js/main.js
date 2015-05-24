cb999d1086134b2eb935b18f5b87a352c415edb3
zhouyichen <zhouyichen123@gmail.com>
2015-05-24 13:53:53 +0800

    Extract functionalities related to reading of xlsx into array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-var xlf = document.getElementById('xlf');
-var xlsxArray;
-var fileReady = false;
-
 var eventNames = {
     '222' : '2×2 Cube',
     '333' : "Rubik's Cube",
@@ -24,7 +20,6 @@ var eventNames = {
 
     '333fm' : 'Fewest Moves' // no need to generate scoresheet for this
 };
-
 
 /**
  * Each Scoresheet represents the info on one scoresheet
@@ -52,47 +47,8 @@ var Generator = function () {
     this.mbf = [];
 }
 
-function handleFile(e) {
-    var files = e.target.files;
-    var file = files[0];
-    process(file);
-}
-
-xlf.addEventListener('change', handleFile, false);
-
-function process(file) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        var data = e.target.result;
-        var arr = fixdata(data);
-        var wb = XLSX.read(btoa(arr), {type: 'base64'});
-        xlsxArray = to_array(wb);
-        fileReady = true;
-    };
-    reader.readAsArrayBuffer(file);
-}
-
-function fixdata(data) {
-    var o = '', l = 0, w = 10240;
-    for(; l<data.byteLength/w; ++l) o+=String.fromCharCode.apply(null,new Uint8Array(data.slice(l*w,l*w+w)));
-    o+=String.fromCharCode.apply(null, new Uint8Array(data.slice(l*w)));
-    return o;
-}
-
-function to_array(workbook) {
-    var result = [];
-    workbook.SheetNames.forEach(function(sheetName) {
-        var csv = XLSX.utils.sheet_to_csv(workbook.Sheets[sheetName]);
-        array = csv.csvToArray({rSep:'\n'});
-        if(csv.length > 0){
-            result[sheetName] = array;
-        }
-    });
-    return result;
-}
-
 function generate(){
-    if (fileReady) {
+    if (xlsxArray) {
         generateFirstRounds(xlsxArray, true);
     }
     else {
