6b3856f8f122564b025d1dc701fec996be3d89d5
zhouyichen <zhouyichen123@gmail.com>
2022-12-12 18:32:22 +0800

    add round formats

---
+++ js/formats.js
@@ -0,0 +1,22 @@
+var formats = {
+    1: {
+        name: "Best of 1",
+        attempts: 1
+    },
+    2: {
+        name: "Best of 2",
+        attempts: 1
+    },
+    3: {
+        name: "Best of 3",
+        attempts: 1
+    },
+    a: {
+        name: "Average of 5",
+        attempts: 5
+    },
+    m: {
+        name: "Mean of 3",
+        attempts: 3
+    }
+};
